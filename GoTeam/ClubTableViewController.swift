3f9d1ab0e82054269deecfad94f61d48dc779168
Brett Ponder <kidunot.tech@gmail.com>
2017-02-17 14:10:52 -0600

    check if the user is logged in and ui fix

--- GoTeam/ClubTableViewController.swift
+++ GoTeam/ClubTableViewController.swift
@@ -117,14 +117,41 @@ class ClubTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "userProfileSegue" {
+            if PFUser.current() != nil {
+                return true
+            } else {
+                let signInAction: UIAlertController = UIAlertController(title: "Hello", message: "You need to sign in or sign up first.", preferredStyle: .alert)
+                let okButton: UIAlertAction = UIAlertAction(title: "OK", style: .cancel, handler: { (alert: UIAlertAction!) -> Void in
+                    
+                })
+                let signInButton: UIAlertAction = UIAlertAction(title: "Sign In", style: .default, handler: { (alert: UIAlertAction!) -> Void in
+                    let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "signNav") as! SignInUpNavViewController
+                    self.present(controller, animated: true, completion: nil)
+                })
+                let signUpButton: UIAlertAction = UIAlertAction(title: "Sign Up", style: .default, handler: { (alert: UIAlertAction!) -> Void in
+                    let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "signNav") as! SignInUpNavViewController
+                    self.present(controller, animated: true, completion: nil)
+                })
+                signInAction.addAction(okButton)
+                signInAction.addAction(signInButton)
+                signInAction.addAction(signUpButton)
+                
+                self.present(signInAction, animated: true, completion: nil)
+                
+                return false
+            }
+        } else {
+            return true
+        }
+    }
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
